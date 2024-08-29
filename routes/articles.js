a486beafd99d30ee96b9fc38bda7781c82dcdbed
Issa laye KANE <issa.k@deally.fr>
2024-08-29 14:19:42 +0000

    add news version

--- routes/articles.js
+++ routes/articles.js
@@ -6,10 +6,10 @@ router.get('/top', require('../controllers/article').topArticle);
 router.get('/article/:slug', require('../controllers/article').slug);
 
 
-router.post('/', require('../middleweares/auth').checkRole('aministrateur'), require('../controllers/article').add);
+router.post('/', require('../middleweares/auth').checkRole('administrateur'), require('../controllers/article').add);
 
-router.put('/active/:id', require('../middleweares/auth').checkRole('aministrateur'), require('../controllers/article').ActiveArticle);
-router.put('/:id', require('../middleweares/auth').checkRole('aministrateur'), require('../controllers/article').update);
+router.put('/active/:id', require('../middleweares/auth').checkRole('administrateur'), require('../controllers/article').ActiveArticle);
+router.put('/:id', require('../middleweares/auth').checkRole('administrateur'), require('../controllers/article').update);
 
 
 
